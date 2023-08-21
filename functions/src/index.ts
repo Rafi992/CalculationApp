86b25cc579877f8b104d52a35b1ec4b782543739
Rafał Bochniewicz <rafal.bochniewicz@gmail.com>
2023-08-21 18:17:41 +0200

    Update index.ts 2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,20 +2,35 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-exports.syncCalculationHistory = functions.firestore
+exports.updateCommunicationData = functions.firestore
   .document("users/{userId}")
-  .onUpdate((change, context) => {
+  .onUpdate(async (change, context) => {
     const newValue = change.after.data();
     const previousValue = change.before.data();
+
+    const userId = context.params.userId;
+    // Pobierz informacje o użytkowniku z Firebase
+    const userRecord = await admin.auth().getUser(userId);
+    const username = userRecord.displayName || "Unknown";
 
     // Sprawdź, czy pole calculationHistory zostało zmienione
     if (newValue.calculationHistory !== previousValue.calculationHistory) {
       const newHistory = newValue.calculationHistory;
 
-      // Aktualizuj kolekcję "communication" z nową historią obliczeń
-      return admin.firestore().collection("communication").doc("data").update({
-        calculationHistory: newHistory,
+      const communicationData = newHistory.map((obj: any) => {
+        return { ...obj, userName: username.split(" ")[0] };
       });
+
+      // Aktualizuj kolekcję "communication" z nową historią obliczeń
+      return admin
+        .firestore()
+        .collection("communication")
+        .doc("data")
+        .update({
+          calculationHistory: admin.firestore.FieldValue.arrayUnion(
+            ...communicationData
+          ),
+        });
     }
     return null;
   });
